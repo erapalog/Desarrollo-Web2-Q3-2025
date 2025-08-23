c5f99277ba5f081afbff918b9d1ad781c2538f1e
errapalog <errapalog@bancatlan.hn>
2025-08-22 18:32:14 -0600

    examen

--- Semana 3/prueba1/src/app/Context/TriviaContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export const triviaContext=createContext({
-    score:0,
-    answeredCount:0,
-    incrementScore :(points:number)=>{},
-    resetGame:()=>{},
-})
--- Semana 3/prueba1/src/app/Modelos/Preguntas.tsx
+++
@@ -1,8 +0,0 @@
-export interface Preguntas {
-    idPregunas: number
-    descripcionPregunta: string
-    opcionRespuesta1: boolean,
-    opcionRespuesta2: boolean,
-    respuestaCorrecta: boolean,
-    puntajePregunta: number
-}
